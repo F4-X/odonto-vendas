a575ec2cb6606febe0dd21fab3157372d71a01f0
F4chini <f4xcore@gmail.com>
2026-05-18 19:12:03 -0300

    aa

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,17 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { CartProvider } from './contexts/CartContext.jsx';
-import './styles/global2.css';
-import './styles/variables.css';
-import './styles/base.css';
-import './styles/header.css';
-import './styles/hero.css';
-import './styles/buttons.css';
-import './styles/sections.css';
-import './styles/products.css';
-import './styles/admin.css';
-import './styles/responsive.css';
-
+import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
